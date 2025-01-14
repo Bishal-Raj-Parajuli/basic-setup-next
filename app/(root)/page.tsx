72c5350056d4b5ce453fe7f1bbf6d14d9ef61dfa
bishal-raj-parajuli <hello76tortugaa@protonmail.com>
2025-01-14 19:00:33 +1100

    feat: install shadcn library

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 const page = () => {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      <Button>Click Me</Button>
+    </div>
+  );
 };
 
 export default page;
