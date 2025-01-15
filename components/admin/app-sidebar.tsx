6bb9a3ea6724190e16ac090ceb90f93db41251ba
bishal-raj-parajuli <hello76tortugaa@protonmail.com>
2025-01-16 09:40:46 +1100

    fix: sidebar mobile error fixe

--- components/admin/app-sidebar.tsx
+++ components/admin/app-sidebar.tsx
@@ -45,7 +45,7 @@ const data = {
       title: "Playground",
       url: "#",
       icon: SquareTerminal,
-      isActive: true,
+      isActive: false,
       items: [
         {
           title: "History",
